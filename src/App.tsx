7ac5c29217e486ed3f3eef222e57dc1b8c2356dd
Danilo Santos <danilocraft933@gmail.com>
2025-07-14 02:07:30 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FC } from "react";
+import Aurora from "./components/Aurora";
+import {
+  GithubLogoIcon,
+  InstagramLogoIcon,
+  LinkedinLogoIcon,
+} from "@phosphor-icons/react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+
+    <div className="w-screen h-screen relative overflow-hidden bg-stone-950 text-white font-nunito">
+
+      <header className="fixed top-4 left-0 right-0 text-center text-xs text-white/50 z-10">
+        © {new Date().getFullYear()} Danilo Moura
+      </header>
+
+      <div className="fixed inset-0 z-10 flex flex-col items-center justify-center text-center px-4">
+        <h1 className="animate-fadeinup text-5xl opacity-90 sm:text-6xl md:text-7xl font-bold  ">
+          Danilo Moura
+        </h1>
+
+        <div className="flex gap-6 mt-6 opacity-80">
+          <a
+            href="https://www.instagram.com/danilosmoura_/"
+            aria-label="Instagram"
+            className="transition-transform opacity-0 animate-fadeinupone duration-200 hover:scale-110 hover:opacity-100"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <InstagramLogoIcon size={24} weight="regular" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/danilosantos-moura/"
+            aria-label="LinkedIn"
+            className="transition-transform opacity-0 animate-fadeinuponefive duration-200 hover:scale-110 hover:opacity-100"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedinLogoIcon size={24} weight="regular" />
+          </a>
+          <a
+            href="https://github.com/DaniDMoura"
+            aria-label="GitHub"
+            className="transition-transform opacity-0 duration-200 animate-fadeinuptwo hover:scale-110 hover:opacity-100"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GithubLogoIcon size={24} weight="regular" />
+          </a>
+        </div>
+      </div>
+
+      <Aurora
+        colorStops={["#f0f0f0", "#f0f0f0", "#f0f0f0"]}
+        blend={1.5}
+        amplitude={0.2}
+        speed={2}
+      />
+    </div>
+  );
+};
+
+export default App;

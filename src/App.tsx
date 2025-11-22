f022fb23fd0f0b37eb2221decf4cd32692b5217c
Danilo Santos <danilocraft933@gmail.com>
2025-11-22 00:25:18 -0300

    Refactorated UI

--- src/App.tsx
+++ src/App.tsx
@@ -8,46 +8,53 @@ import {
 
 const App: FC = () => {
   return (
-
-
     <div className="w-screen h-screen relative overflow-hidden bg-stone-950 text-white">
-
-      <header className="fixed top-4 left-0 right-0 text-center text-xs text-white/50 z-10">
+      <header className="fixed top-4 left-0 right-0 text-center text-[9px] text-white/50 z-10">
         © {new Date().getFullYear()} Danilo Moura
       </header>
 
       <div className="fixed inset-0 z-10 flex flex-col items-center justify-center text-center px-4">
-        <h1 className="animate-fadeinup text-5xl opacity-90 sm:text-6xl md:text-7xl font-family-primary ">
-          Danilo Moura
-        </h1>
+        <img
+          src="/picture.jpg"
+          alt="Picture"
+          className="w-30 h-30 rounded-full m-5"
+        />
+        <div>
+          <h1 className="text-3xl opacity-90 sm:text-4xl md:text-5xl font-family-primary font-extralight">
+            Danilo Moura
+          </h1>
+          <p className="  font-extralight text-[11px] sm:text-[12px] md:text-[13px]  mt-2">
+            Web Developer
+          </p>
+        </div>
 
-        <div className="flex gap-6 mt-6 opacity-80">
+        <div className="flex flex-col justify-center items-center gap-5 mt-7 opacity-80">
           <a
-            href="https://www.instagram.com/danilosmoura_/"
-            aria-label="Instagram"
-            className="transition-transform opacity-0 animate-fadeinupone duration-200 hover:scale-110 hover:opacity-100"
             target="_blank"
             rel="noopener noreferrer"
+            href="https://www.instagram.com/danilosmoura_"
+            className="hover:bg-white hover:text-black  duration-300 font-family-primary cursor-pointer flex items-center justify-center-safe gap-2 w-60 h-12 text-[11px] border rounded-full shadow-2xl"
           >
-            <InstagramLogoIcon size={24} weight="regular" />
+            <InstagramLogoIcon size={15} />
+            Instagram
           </a>
           <a
+            target="_blank"
+            rel="noopener noreferrer"
             href="https://www.linkedin.com/in/danilosantos-moura/"
-            aria-label="LinkedIn"
-            className="transition-transform opacity-0 animate-fadeinuponefive duration-200 hover:scale-110 hover:opacity-100"
-            target="_blank"
-            rel="noopener noreferrer"
+            className="hover:bg-white hover:text-black duration-300 font-family-primary  cursor-pointer flex items-center justify-center-safe gap-2 w-60 h-12 text-[11px] border rounded-full shadow-2xl"
           >
-            <LinkedinLogoIcon size={24} weight="regular" />
+            <LinkedinLogoIcon size={15} />
+            LinkedIn
           </a>
           <a
-            href="https://github.com/DaniDMoura"
-            aria-label="GitHub"
-            className="transition-transform opacity-0 duration-200 animate-fadeinuptwo hover:scale-110 hover:opacity-100"
             target="_blank"
             rel="noopener noreferrer"
+            href="https://github.com/DaniDMoura"
+            className="hover:bg-white hover:text-black duration-300 font-family-primary cursor-pointer flex items-center justify-center-safe gap-2 w-60 h-12 text-[11px] border rounded-full shadow-2xl"
           >
-            <GithubLogoIcon size={24} weight="regular" />
+            <GithubLogoIcon size={15} />
+            GitHub
           </a>
         </div>
       </div>
